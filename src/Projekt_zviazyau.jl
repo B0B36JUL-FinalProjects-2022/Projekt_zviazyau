48f3bbbcdb5f5f291531e82039e05af37f36bc27
ezvezdov <ezvezdov22@gmail.com>
2023-02-11 12:59:54 +0100

    Transpose X,y matrices, add ReLU and SimpleNet

--- src/Projekt_zviazyau.jl
+++ src/Projekt_zviazyau.jl
@@ -1,6 +1,6 @@
 module Projekt_zviazyau
 
-export transform_data,preview_image
+export transform_data,preview_image, SimpleNet
 
 using CSV
 using DataFrames
@@ -25,26 +25,41 @@ function transform_data(path::String)
 
     select!(df, Not(:Image))
     
-    Y = Matrix(df)
-    Y = (Y .- IMAGE_HALFSIZE) ./ IMAGE_HALFSIZE # scale to [-1,1]
-    return X, Y
+    y = Matrix(df)
+    y = (y .- IMAGE_HALFSIZE) ./ IMAGE_HALFSIZE # scale to [-1,1]
+    return X', y'
 end
 
-function preview_image(X,Y,image_num; flip = true)
+function preview_image(X,y,image_num; flip = true)
     
     # Reshaping an flipping image
-    current_image = reshape(X[image_num,:],96,96);
+    current_image = reshape(X[:,image_num],IMAGE_SIZE,IMAGE_SIZE);
     current_image = flip ? PermutedDimsArray(current_image, (2, 1)) : current_image
     
     # Reshaping keypoints
-    current_keypoints = Y[image_num,:]
+    current_keypoints = y[:,image_num]
     current_keypoints = reshape(current_keypoints,2,15)
-    current_keypoints = (current_keypoints .* IMAGE_HALFSIZE) + IMAGE_HALFSIZE
+    current_keypoints = (current_keypoints .* IMAGE_HALFSIZE) .+ IMAGE_HALFSIZE
 
     # Plot data
     plot(Gray.(reshape(current_image,IMAGE_SIZE,IMAGE_SIZE)))
     plot!(current_keypoints[1,:], current_keypoints[2,:], seriestype=:scatter, label="Face keypoints")
     
 end
+
+struct SimpleNet{T<:Real}
+    W1::Matrix{T}
+    b1::Vector{T}
+    W2::Matrix{T}
+    b2::Vector{T}
+end
+
+SimpleNet(n1, n2, n3) = SimpleNet(randn(n2, n1), randn(n2), randn(n3, n2), randn(n3))
+
+function ReLU(Z)
+    return max.(0,Z)
+end
+
+
 
 end # module Projekt_zviazyau
