b761e3556b0d38d23f691af11cd17e15103b86cb
ezvezdov <ezvezdov22@gmail.com>
2023-02-13 12:05:35 +0100

    Add Xavier init

--- src/Projekt_zviazyau.jl
+++ src/Projekt_zviazyau.jl
@@ -1,32 +1,30 @@
 module Projekt_zviazyau
 
-export SimpleNet,train
+export SimpleNet,train #, simplenet_grad, ReLU, d_ReLU, id, d_id, MSE, d_MSE
 
 include("utils.jl")
-
 
 using LinearAlgebra
 using Colors, Plots
 using Statistics
 
-
 # ReLU activation function
-function ReLU(Z)
-    return max.(0,Z)
-end
+ReLU(Z) = max.(0,Z)
 
 # Derivative of ReLU activation function
-function d_ReLU(Z)
-    return Z .> 0
-end
+d_ReLU(Z) =  Z .> 0
 
 # id activation function
+id(Z) = Z
 
-function id(Z)
-    return Z
-end
+# Derivative of id function
+d_id(Z) = ones(size(Z))
 
+# Mean Squared Error loss function
+MSE(a2,y) = sum((a2 .- y ).^2,dims=2) / size(y,2)
 
+# Derivative of MSE function
+d_MSE(a2,y) = (2 *(a2 .- y))
 
 # Simple Neural Network with 1 hidden dense layer
 struct SimpleNet{T<:Real}
@@ -36,72 +34,79 @@ struct SimpleNet{T<:Real}
     b2::Vector{T}
 end
 
-# Constructor for SimpleNet function
-SimpleNet(n1, n2, n3) = SimpleNet(randn(n2, n1), randn(n2), randn(n3, n2), randn(n3))
+# Constructor for SimpleNet function with normal distribution initialization
+# SimpleNet(n1, n2, n3) = SimpleNet(randn(n2, n1), zeros(n2), randn(n3, n2), zeros(n3))
+
+# Constructor for SimpleNet function with Xavier initialization
+SimpleNet(n1, n2, n3) = SimpleNet(rand(-sqrt(6/(n2+n1)):sqrt(6/(n2+n1)),n2,n1), zeros(n2), rand(-sqrt(6/(n3+n2)):sqrt(6/(n3+n2)),n3,n2), zeros(n3))
 
 # SimpleNet prediction functor
 function (m::SimpleNet)(x)
     z1 = m.W1*x .+ m.b1
-    a1 = ReLU(z1);
+    a1 = ReLU(z1)
     z2 = m.W2*a1 .+ m.b2
     a2 = id(z2)
     return a2
 end
 
-function simplenet_grad(m::SimpleNet, x::AbstractVector, y; ϵ=1e-10)
+function simplenet_grad(m::SimpleNet, x, y; ϵ=1e-10)
+    samples_amount = size(y,2) # SA
+
     z1 = m.W1*x .+ m.b1
+    #100xSA =  100x9216 * 9216xSA .+ 100xSA
+
     a1 = ReLU(z1)
+    # 100xSA = 100xSA
+
     z2 = m.W2*a1 .+ m.b2
+    #30xSA = 30x100 * 100xSA + 30xSA
+
     a2 = id(z2)
+    #30xSA = 30xSA
 
     # Place for loss function
-    # l = -sum(y .* log.(a2 .+ ϵ))
+    loss = MSE(a2,y)
 
-    # e_z2 = exp.(z2)
-    # l_part = (- e_z2 * e_z2' + Diagonal(e_z2 .* sum(e_z2))) / sum(e_z2)^2
+    d_a2 = d_MSE(a2,y)
+    # 30xSA = 30xSA
 
-    d_z2 = a2 - y
-    d_w2 = d_z2 * a1'
-    d_b2 = d_z2
-    d_z1 = m.W2' * d_z2 .* d_ReLU(z1)
-    d_w1 = d_z1 * x'
-    d_b1 = d_z1
+    d_z2 = d_a2 .* d_id(a2)
+    # 30xSA = 30xSA .* 30xSA
 
+    d_a1 = m.W2' * d_z2
+    # 100xSA = 100x30 * 30xSA
 
-    # l_a2 = - y ./ (a2 .+ ϵ)
-    # l_z2 = l_a2 # l_part * l_a2
-    # l_a1 = m.W2' * l_z2
-    # l_z1 = l_a1 .* (a1 .> 0)
-    # l_x = m.W1' * l_z1
+    d_z1 = d_a1 .* d_ReLU(z1)
+    # 100xSA = 100xSA .* 100xSA
 
-    # l_W2 = l_z2 * a1'
-    # l_b2 = l_z2
-    # l_W1 = l_z1 * x'
-    # l_b1 = l_z1
+    d_w2 = (d_z2 * a1') / samples_amount
+    # 30x100 = 30xSA * SAx100
+    d_b2 = sum(d_z2,dims=2) / samples_amount
+    # 30x1 = 30x1
+    d_w1 = (d_z1 * x') / samples_amount
+    # 100x9216 = 100xSA * SAx9216
+    d_b1 = sum(d_z1,dims=2) / samples_amount
+    # 100x1 = 100x1
 
-    # return l, l_W1, l_b1, l_W2, l_b2
-    return d_w1, d_b1, d_w2, d_b2
+    return d_w1, d_b1, d_w2, d_b2, loss
 end
 
-function mean_tuple(d::AbstractArray{<:Tuple})
-    Tuple([mean([d[k][i] for k in 1:length(d)]) for i in 1:length(d[1])])
-end
-
-
-function train(m,X_train,y_train)
+function train(m,X_train,y_train;epoch = 1000)
     alpha = 1e-2
-    max_iter = 2000
-    # L = zeros(max_iter)
-    for iter in 1:max_iter
-        grad_all = [simplenet_grad(m, X_train[:,k], y_train[:,k]) for k in 1:size(X_train,2)]
-        grad_mean = mean_tuple(grad_all)
+    L = zeros(epoch)
 
-        # L[iter] = grad_mean[1]
+    for cur_epoch in 1:epoch
+        grad = simplenet_grad(m,X_train,y_train)
+        L[cur_epoch] = sum(grad[5])
 
-        m.W1 .-= alpha*grad_mean[1]
-        m.b1 .-= alpha*grad_mean[2]
-        m.W2 .-= alpha*grad_mean[3]
-        m.b2 .-= alpha*grad_mean[4]
+        m.W1 .-= alpha*grad[1]
+        m.b1 .-= alpha*grad[2]
+        m.W2 .-= alpha*grad[3]
+        m.b2 .-= alpha*grad[4]
+
+        if cur_epoch % 50 == 0 || cur_epoch == 1
+            println("MSE: ", L[cur_epoch])
+        end
     end
 end
 
