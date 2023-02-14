27862372188daa5ae6181d9cb5d6d414e8f4af78
ezvezdov <ezvezdov22@gmail.com>
2023-02-14 18:12:32 +0100

    Add merge_data(), refactor make_submission()

--- src/utils.jl
+++ src/utils.jl
@@ -1,4 +1,4 @@
-export transform_train_data,transform_test_data,make_submission, preview_image
+export transform_train_data,transform_test_data,merge_data, make_submission, preview_image
 
 using CSV
 using DataFrames
@@ -7,23 +7,23 @@ IMAGE_SIZE = 96
 IMAGE_HALFSIZE = 48
 COLOR_MAX = 255
 
-Keypoints_type = Dict(
-    "left_eye_center_x" => 1,"left_eye_center_y" => 2,
-    "right_eye_center_x" => 3,"right_eye_center_y" => 4,
-    "left_eye_inner_corner_x" => 5,"left_eye_inner_corner_y" => 6,
-    "left_eye_outer_corner_x" => 7,"left_eye_outer_corner_y" => 8,
-    "right_eye_inner_corner_x" => 9,"right_eye_inner_corner_y" => 10,
-    "right_eye_outer_corner_x" => 11,"right_eye_outer_corner_y" => 12,
-    "left_eyebrow_inner_end_x" => 13,"left_eyebrow_inner_end_y" => 14,
-    "left_eyebrow_outer_end_x" => 15,"left_eyebrow_outer_end_y" => 16,
-    "right_eyebrow_inner_end_x" => 17,"right_eyebrow_inner_end_y" => 18,
-    "right_eyebrow_outer_end_x" => 19,"right_eyebrow_outer_end_y" => 20,
-    "nose_tip_x" => 21,"nose_tip_y" => 22,
-    "mouth_left_corner_x" => 23,"mouth_left_corner_y" => 24,
-    "mouth_right_corner_x" => 25,"mouth_right_corner_y" => 26,
-    "mouth_center_top_lip_x" => 27,"mouth_center_top_lip_y" => 28,
-    "mouth_center_bottom_lip_x" => 29,"mouth_center_bottom_lip_y" => 30
-)
+keypoints_types = [
+    "left_eye_center_x","left_eye_center_y",
+    "right_eye_center_x","right_eye_center_y",
+    "left_eye_inner_corner_x","left_eye_inner_corner_y",
+    "left_eye_outer_corner_x","left_eye_outer_corner_y",
+    "right_eye_inner_corner_x","right_eye_inner_corner_y",
+    "right_eye_outer_corner_x","right_eye_outer_corner_y",
+    "left_eyebrow_inner_end_x","left_eyebrow_inner_end_y",
+    "left_eyebrow_outer_end_x","left_eyebrow_outer_end_y",
+    "right_eyebrow_inner_end_x","right_eyebrow_inner_end_y",
+    "right_eyebrow_outer_end_x","right_eyebrow_outer_end_y",
+    "nose_tip_x","nose_tip_y",
+    "mouth_left_corner_x","mouth_left_corner_y",
+    "mouth_right_corner_x","mouth_right_corner_y",
+    "mouth_center_top_lip_x","mouth_center_top_lip_y",
+    "mouth_center_bottom_lip_x","mouth_center_bottom_lip_y"
+]
 
 """ Parse image date to float and scale image colors from [0,255] to [0,1] """
 function transform_image(df)
@@ -51,8 +51,7 @@ function transform_train_data(path::String)
 
     keys4 = String[]
     keys11 = String[]
-    keypoinst_keys = keys(Keypoints_type)
-    for key in keypoinst_keys
+    for key in keypoints_types
         # Remove column if more than 1% is missing
         if sum(ismissing.(df[:,key]))/samples_count > 1e-2
             select!(df4, Not(key))
@@ -70,7 +69,7 @@ function transform_train_data(path::String)
     select!(df4, Not(:Image))
     y4 = trainsform_keypoints(df4)
 
-    df11 = dropmissing(df)
+    df11 = dropmissing(df11)
     X11 = transform_image(df11);
     select!(df11, Not(:Image))
     y11 = trainsform_keypoints(df11)
@@ -81,26 +80,36 @@ end
 """ Prepare test data. """
 function transform_test_data(path::String)
     df = DataFrame(CSV.File(path))
-    # TODO: REMOVE
-    ######################
-    # df = first(df, 10)
-    ######################
     X = transform_image(df);
     return X
 end
 
+""" Merge prediction splits. """
+function merge_data(y1,keys1,y2,keys2)
+    keys_all = vcat(keys1,keys2)
+    y = vcat(y1,y2)
+    return y, keys_all
+end
+
 
 """ Make submission for kaggle Facial Keypoints Detection competition. """
-function make_submission(IdLookupTable_path::String,y)
+function make_submission(IdLookupTable_path::String,y, keys_all)
     df = DataFrame(CSV.File(IdLookupTable_path))
     select!(df, Not(:Location)) # Remove Location column (because of missing)
     df[!,:Location] = zeros(size(df,1)) # Add new Location column with zeros
 
-    # Scale back y to [0,96]
+    # Scale back y to [0,96] and round to integer value
     y = (y .* IMAGE_HALFSIZE) .+ IMAGE_HALFSIZE
+    y = round.(y)
 
-    for i in 1:size(df,1)
-        df[i,:].Location = y[:,df[i,:].ImageId][Keypoints_type[df[i,:].FeatureName]]
+    # Transform data into DataFrame
+    df_pred = DataFrame(y', keys_all)
+
+    # Fill .Location column
+    for row in eachrow(df)
+        image_id = row.ImageId
+        feature_name = row.FeatureName
+        row.Location = df_pred[image_id,feature_name]
     end
 
     # Save data to "kaggle_data" folder
